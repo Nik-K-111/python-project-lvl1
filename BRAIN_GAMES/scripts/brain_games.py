335863185d6346478ad6a39b1c655c2e2aed4e79
Nik-K-111 <nik.korenga.111@gmail.com>
2021-11-13 23:51:01 +0700

    redaction by step=3

---
+++ BRAIN_GAMES/scripts/brain_games.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python3
+
+from BRAIN_GAMES.cli import welcome_user
+
+def welcome():
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
+def main():
+    welcome()
+
+if __name__ == '__main__':
+    main()
+
