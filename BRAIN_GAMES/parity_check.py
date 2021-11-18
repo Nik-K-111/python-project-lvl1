ab61bfbebd15b91466856e7ba8afe10e1b582be1
Nik-K-111 <nik.korenga.111@gmail.com>
2021-11-18 23:07:25 +0700

    add files of brain-even

---
+++ BRAIN_GAMES/parity_check.py
@@ -0,0 +1,47 @@
+#! usr/bin/env python3
+
+import prompt
+import random
+
+def parity_check():    
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+
+    i = 3
+    while i > 0:
+        i = i - 1
+        proposed_number = random.randint(11, 99)
+
+        if proposed_number & 1 == 1:
+            correct_answer = 'no'
+        else:
+            correct_answer = 'yes'
+
+        print('Question: {}'.format(proposed_number))
+        user_answer = prompt.string('Your answer: ')
+        
+        if user_answer == correct_answer:
+            result = True
+            print('Correct!')
+
+        else:
+            result = False
+            print(
+                f"'{user_answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'.\n"
+                f"Let's try again, {name}!"
+            )
+
+            i = 0
+
+    if result == True:
+        print(f"Congratulations, {name}!")
+
+
+def main():
+    parity_check()
+
+
+if __name__ == '__main__':
+    main()
---
+++ BRAIN_GAMES/scripts/brain_even.py
@@ -0,0 +1,16 @@
+#! usr/bin/env python3
+
+from BRAIN_GAMES.parity_check import parity_check
+
+
+def brain_even():
+    print("Welcome to the Brain Games!")
+    parity_check()
+
+
+def main():
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
