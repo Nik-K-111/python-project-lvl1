bd8f6d23bbbd10b4940fa30a5d3b9981bb43c276
Nik-K-111 <nik.korenga.111@gmail.com>
2021-12-14 00:44:24 +0700

    definition function is_even()

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,13 +6,19 @@ REGULATIONS = 'Answer "yes" if the number is even, otherwise answer "no"'
 ROUNDS = 3
 
 
+def is_even(number):
+    if number & 1 == 1:
+        return False
+    return True
+
+
 def current_game():
 
     MIN_VALUE, MAX_VALUE = 11, 99
 
     question = random.randint(MIN_VALUE, MAX_VALUE)
 
-    if question & 1 == 1:
+    if is_even(question):
         correct_answer = 'no'
     else:
         correct_answer = 'yes'
