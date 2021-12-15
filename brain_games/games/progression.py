747e7aed5f512afa27e760cd549c1efd96eba845
Nik-K-111 <nik.korenga.111@gmail.com>
2021-12-16 03:25:38 +0700

    исправление синтаксиса progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -7,15 +7,18 @@ ROUNDS = 3
 
 
 def current_game():
-    progression_string = ''
     NUMBER_OF_MEMBERS = 10
-    list_1_term = [2, 3, 4, 6, 7, 8, 9, 12, 13, 14, 16, 17, 18, 19]
-    [term] = random.sample(list_1_term, 1)
-    difference = random.randint(3, 9)
-    n_unknown_term = random.randint(1, 10)
+    MIN_DIFFERENCE, MAX_DIFFERENCE = 3, 9
+    MIN_X_TERM, MAX_X_TERM = 1, 10
+    LIST_FIRST_TERM = [2, 3, 4, 6, 7, 8, 9, 12, 13, 14, 16, 17, 18, 19]
+    progression_string = ''
+
+    [term] = random.sample(LIST_FIRST_TERM, 1)
+    difference = random.randint(MIN_DIFFERENCE, MAX_DIFFERENCE)
+    num_x_term = random.randint(MIN_X_TERM, MAX_X_TERM)
     j = NUMBER_OF_MEMBERS
     while j > 0:
-        if j != n_unknown_term:
+        if j != num_x_term:
             progression_string = progression_string + str(term) + ' '
         else:
             correct_answer = term
